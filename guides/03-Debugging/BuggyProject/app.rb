5f38490160781bd198b72d09553c8093cde0dda9
Chloe <chloe.wong@madetech.com>
2020-10-08 11:42:26 +0100

    added puts statements to app.rb

--- guides/03-Debugging/BuggyProject/app.rb
+++ guides/03-Debugging/BuggyProject/app.rb
@@ -1,19 +1,21 @@
 require 'sinatra'
 require 'redcarpet'
 require_relative 'lib/view_topic'
+require_relative 'lib/list_topics'
 
 get '/' do
-  response = List.new.execute(directory: "#{__dir__}/../../")
-
+  response = ListTopics.new.execute(directory: "#{__dir__}/../../")
+  puts "response: #{response}"
   output = '<h1>Learning Topics</h1>'
   output << '<ul>'
   response[:topics].each do |topic|
     output << "<li><a href=\"/topic/#{topic[:id]}\">#{topic[:title]}</a></li>"
   end
   output << '</ul>'
-
+  puts "output: #{output}"
   output
 end
+
 
 get '/topic/:id' do
   response = ViewTopic.new.execute(
